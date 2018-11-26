08a5472f65131f9122f60c01caa8f8987b9bf643
Tuula Yoshinari <tuula.yoshinari@stud.pointcollege.fi>
2018-11-26 11:38:18 +0200

    Toteutettu lämpötilan kirjoittaminen ja tallentaminen tiedostoon.

--- TiedonTallennus/TiedonTallennus/Program.cs
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace TiedonTallennus
     {
         static void Main(string[] args)
         {
+            int lämpötila = 0; //koska konsolisovellus pyydetään käyttäjää kirjoittamaan haluttu lämpötila
+            Console.Write("Anna uusi lämpötila: "); //staattisia metodeja
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte); //muuttuja
+
+            //tiedostoon kirjoittaminen
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            //tiedostopolut HUOM laita @ niin tarkoittaa että escape merkit eivät käytössä, ja VS tunnistaa ne tiedostopoluksi
+            //Tallentaa muuttujan tiedostoon, File. jälkeen paina lamppua ja ota System.IO käyttöön
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            Console.ReadLine();
         }
     }
 }
