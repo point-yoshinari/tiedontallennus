e15f1ed31dfa8d29dc3e0377fe42bf951187878c
Tuula Yoshinari <tuula.yoshinari@stud.pointcollege.fi>
2018-11-26 12:04:01 +0200

    Lisätty aiemman lämpötilan lukeminen tiedostosta.

--- TiedonTallennus/TiedonTallennus/Program.cs
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -11,17 +11,32 @@ namespace TiedonTallennus
     {
         static void Main(string[] args)
         {
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+
+            //luetaan tiedostosta aiemmin asetettu lämpötila
+            //sovelluksen pitää pystyä hallitsemaan myös se ettei tiedostoa ole olemassa
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu lämpötila on: " + aiempiArvo + " astetta.");
+
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa!"); //WriteLine tekee myös rivinvaihdon
+            }
+
+
             int lämpötila = 0; //koska konsolisovellus pyydetään käyttäjää kirjoittamaan haluttu lämpötila
-            Console.Write("Anna uusi lämpötila: "); //staattisia metodeja
+            Console.Write("Anna uusi lämpötila: "); //staattisia metodeja, Write pyytää 
             string syöte = Console.ReadLine();
             lämpötila = int.Parse(syöte); //muuttuja
 
             //tiedostoon kirjoittaminen
-            string tiedosto = @"C:\Temp\Lämpötila.txt";
             //tiedostopolut HUOM laita @ niin tarkoittaa että escape merkit eivät käytössä, ja VS tunnistaa ne tiedostopoluksi
             //Tallentaa muuttujan tiedostoon, File. jälkeen paina lamppua ja ota System.IO käyttöön
             File.WriteAllText(tiedosto, lämpötila.ToString());
-            Console.WriteLine("Uusi lämpötila tallennettu.");
+            Console.WriteLine("Uusi lämpötila tallennettu. ");
 
             Console.ReadLine();
         }
